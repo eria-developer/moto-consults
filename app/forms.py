db4c61bff3fca3d18d19ef32daa832e9a8c902fe
eria-developer <eriddeveloper@gmail.com>
2024-06-19 14:06:02 +0300

    added company views

--- app/forms.py
+++ app/forms.py
@@ -13,8 +13,82 @@ class EditCustomerForm(forms.ModelForm):
         model = models.Customer
         fields = "__all__"
 
+        widgets = {
+            "firstname": forms.TextInput(attrs={
+                "class": "form-control",
+            }),
+            "othernames": forms.TextInput(attrs={
+                "class": "form-control",
+            }),
+            "phonenumber_1": forms.TextInput(attrs={
+                "class": "form-control",
+            }),
+            "phonenumber_2": forms.TextInput(attrs={
+                "class": "form-control",
+            }),
+            "address": forms.TextInput(attrs={
+                "class": "form-control",
+            }),
+            "email": forms.EmailInput(attrs={
+                "class": "form-control",
+            }),
+            "passport_photo": forms.FileInput(attrs={
+                "class": "form-control",
+            }),
+            "file_upload": forms.FileInput(attrs={
+                "class": "form-control",
+            }),
+            "remarks": forms.Textarea(attrs={
+                "class": "form-control",
+            }),
+        }
 
-class CustomerSearchForm(forms.Form):
+
+class CompanySearchForm(forms.Form):
     first_name = forms.CharField(required=False, label='First Name')
     last_name = forms.CharField(required=False, label='Last Name')
     email = forms.EmailField(required=False, label='Email')
+
+
+
+
+# FORMS FOR COMPANY START 
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = models.EmployerCompany
+        fields = "__all__"
+
+
+class EditCompanyForm(forms.ModelForm):
+    class Meta:
+        model = models.EmployerCompany
+        fields = "__all__"
+
+        widgets = {
+            "name": forms.TextInput(attrs={
+                "class": "form-control",
+            }),
+            "phonenumber": forms.TextInput(attrs={
+                "class": "form-control",
+            }),
+            "address": forms.TextInput(attrs={
+                "class": "form-control",
+            }),
+            "email": forms.EmailInput(attrs={
+                "class": "form-control",
+            }),
+            "description": forms.Textarea(attrs={
+                "class": "form-control",
+            }),
+        }
+
+
+# class CompanySearchForm(forms.Form):
+#     first_name = forms.CharField(required=False, label='First Name')
+#     last_name = forms.CharField(required=False, label='Last Name')
+#     email = forms.EmailField(required=False, label='Email')
+
+
+# FORMS FOR COMPANY END 
--- app/urls.py
+++ app/urls.py
@@ -11,4 +11,12 @@ urlpatterns = [
     path("delete-customer/<int:customer_id>/", views.delete_customer, name="delete-customer"),
     path("list-of-customers/", views.list_of_customers, name="list-of-customers"),
     path("search-customer/", views.search_customer, name="search-customer"),
+
+    # company urls 
+    # path("add-company/", views.add_company, name="add-company"),
+    # path("edit-company/<int:company_id>/", views.edit_company, name="edit-company"),
+    # path("view-company/<int:company_id>/", views.view_company, name="view-company"),
+    # path("delete-company/<int:company_id>/", views.delete_company, name="delete-company"),
+    # path("list-of-companies/", views.list_of_companies, name="list-of-companies"),
+    # path("search-company/", views.search_company, name="search-company"),
 ]
