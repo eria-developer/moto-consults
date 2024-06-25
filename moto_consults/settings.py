30beea9676d032476d6ae5b9339e0d8eddd06caa
eria-developer <eriddeveloper@gmail.com>
2024-06-25 19:53:43 +0300

    changed db to the local one for dev

--- moto_consults/settings.py
+++ moto_consults/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'moto_consults.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -96,9 +96,9 @@ WSGI_APPLICATION = 'moto_consults.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES = {
+#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 
 # Password validation
