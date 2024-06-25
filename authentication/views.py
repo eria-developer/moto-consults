bfac07cb1f179009522435e464ed116cab97acb8
eria-developer <eriddeveloper@gmail.com>
2024-06-25 20:35:41 +0300

    implememnted login and signup

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('list-of-customers')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
