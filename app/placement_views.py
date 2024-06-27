d363201a6f79b16a7d7d59a04b7ee5727da58a10
eria-developer <eriddeveloper@gmail.com>
2024-06-27 11:57:07 +0300

    implemented pop up models on add placement page

--- app/placement_views.py
+++ app/placement_views.py
@@ -10,19 +10,59 @@ from django.http import JsonResponse
 
 def add_placement(request):
     if request.method == "POST":
-        form = forms.PlacementForm(request.POST)
-        if form.is_valid():
-            print(form)
-            form.save()
-            messages.success(request, "Placement added successfully")
-            return redirect("list-of-placements")
-        else:
-            for field, errors in form.errors.items():
-                for error in errors:
-                    messages.error(request, f"{error} in {field}")
-                    print(f"Erros are: {error} in {field}")
+        form_name = request.POST.get('form_name')
+
+        if form_name == "add_placement_form":
+            form = forms.PlacementForm(request.POST)
+            if form.is_valid():
+                print(form)
+                form.save()
+                messages.success(request, "Placement added successfully")
+                return redirect("list-of-placements")
+            else:
+                for field, errors in form.errors.items():
+                    for error in errors:
+                        messages.error(request, f"{error} in {field}")
+                        print(f"Erros are: {error} in {field}")
+        
+        elif form_name == "add_customer_form":
+            form = forms.CustomerForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Customer added successfully")
+                return redirect("add-placement")
+            else:
+                for field, errors in form.errors.items():
+                    for error in errors:
+                        messages.error(request, f"{error} in {field}")
+                        
+        elif form_name == "add_company_form":
+            add_company_form = forms.CompanyForm(request.POST)
+            if add_company_form.is_valid():
+                add_company_form.save()
+                messages.success(request, "Company added successfully")
+                return redirect("add-placement")
+            else:
+                for field, errors in add_company_form.errors.items():
+                    for error in errors:
+                        messages.error(request, f"{error} in {field}")
+                        print(f"{error} in {field}")
+        elif form_name == "add_job_form":
+            add_job_form = forms.JobForm(request.POST)
+            if add_job_form.is_valid():
+                add_job_form.save()
+                messages.success(request, "Job added successfully")
+                return redirect("add-placement")
+            else:
+                for field, errors in add_job_form.errors.items():
+                    for error in errors:
+                        messages.error(request, f"{error} in {field}")
+                        print(f"{error} in {field}")
     else:
-        form = forms.PlacementForm()
+        add_placement_form = forms.PlacementForm()
+        add_customer_form = forms.CustomerForm()
+        add_job_form = forms.JobForm()
+        add_company_form = forms.CompanyForm()
 
     customers = models.Customer.objects.all()
     jobs = models.Job.objects.all()
@@ -30,7 +70,10 @@ def add_placement(request):
     job_companies = models.EmployerCompany.objects.all()
 
     context = {
-        "form": form,
+        "add_placement_form": add_placement_form,
+        "add_customer_form": add_customer_form,
+        "add_job_form": add_job_form,
+        "add_company_form": add_company_form,
         "customers": customers,
         "jobs": jobs,
         "status_choices": status_choices,
