d6d530bcaac44ef8c824c218ee234f24a97733c5
eria-developer <eriddeveloper@gmail.com>
2024-07-06 15:35:14 +0300

    polished the expenses module

--- app/expense_views.py
+++ app/expense_views.py
@@ -29,6 +29,7 @@ def add_expense(request):
 @login_required(login_url="/")
 def list_of_expenses(request):
     expenses = models.Expense.objects.all().order_by("-date_added")
+    my_expenses = models.Expense.objects.filter(user=request.user).order_by("-date_added")
     add_expense_form = forms.ExpenseForm()
 
     if request.method == "POST":
@@ -46,5 +47,33 @@ def list_of_expenses(request):
     context = {
         "expenses": expenses,
         "add_expense_form": add_expense_form,
+        "my_expenses": my_expenses,
     }
     return render(request, "list_of_expenses.html", context)
+
+
+@login_required(login_url="/")
+def list_of_expenses_of_all_users(request):
+    if request.user.role == "admin":
+        expenses = models.Expense.objects.all().order_by("-date_added")
+        add_expense_form = forms.ExpenseForm()
+
+        if request.method == "POST":
+            add_expense_form = forms.ExpenseForm(request.POST)
+            if add_expense_form.is_valid():
+                add_expense_form.save()
+                messages.success(request, "Expense added successfully!!")
+                return redirect("list-of-expenses")
+            else:
+                for errors, field in add_expense_form.errors.items():
+                    for error in errors:
+                        messages.error(request, f"{error} in {field}")
+                        print(f"errors: {error} in {field}")
+    else:
+        return redirect("list-of-expenses")
+    
+    context = {
+        "expenses": expenses,
+        "add_expense_form": add_expense_form,
+    }
+    return render(request, "list_of_expenses copy_for_all_users.html", context)
--- app/urls.py
+++ app/urls.py
@@ -73,5 +73,6 @@ urlpatterns = [
 
     # expenses urls 
     path("add-expense/", views.add_expense, name="add-expense"),
-    path("list-of-expenses/", views.list_of_expenses, name="list-of-expenses")
+    path("list-of-expenses/", views.list_of_expenses, name="list-of-expenses"),
+    path("list-of-expenses-for-all-users/", views.list_of_expenses_of_all_users, name="list-of-expenses-for-all-users")
 ]
